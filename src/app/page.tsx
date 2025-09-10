8f2bc631cfae11355ed2991d92cb17a937ae13c4
Xyber Nest <xybernest60@gmail.com>
2025-09-10 10:04:38 +0000

    Try fixing this error: `Build Error: Export Road doesn't exist in target

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Car, TrafficCone, Road, TrafficLight as TrafficLightIcon } from "lucide-react";
+import { Car, TrafficCone, Waypoints, TrafficLight as TrafficLightIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { TrafficPilotIcon } from "@/components/icons/traffic-pilot-icon";
@@ -27,7 +27,7 @@ export default function LandingPage() {
             <div className="absolute inset-0 z-0">
                 <FloatingIcon icon={Car} className="top-[10%] left-[15%] text-4xl" />
                 <FloatingIcon icon={TrafficLightIcon} className="top-[20%] right-[20%] text-5xl" />
-                <FloatingIcon icon={Road} className="bottom-[15%] left-[25%] text-6xl" />
+                <FloatingIcon icon={Waypoints} className="bottom-[15%] left-[25%] text-6xl" />
                 <FloatingIcon icon={TrafficCone} className="bottom-[20%] right-[15%] text-4xl" />
                 <FloatingIcon icon={Car} className="top-[60%] left-[30%] text-5xl" />
                 <FloatingIcon icon={TrafficLightIcon} className="top-[70%] right-[40%] text-3xl" />
